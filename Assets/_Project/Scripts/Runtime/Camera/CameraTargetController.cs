9bab5596ff61221fcf6178758810c66f96e6c9e2
ECortus <ve.cortus@gmail.com>
2025-11-17 20:18:10 +0200

    upd

---
+++ Assets/_Project/Scripts/Runtime/Camera/CameraTargetController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+using UnityEditor;
+
+namespace PanzerHero.Runtime
+{
+#if UNITY_EDITOR
+    [InitializeOnLoad]
+#endif
+    public class CameraTargetController : MonoBehaviour
+    {
+        [SerializeField] private Transform overrideTarget;
+        
+        void Start()
+        {
+            SetCinemachineTarget();
+        }
+
+        void SetCinemachineTarget()
+        {
+            var cinemachine = FindAnyObjectByType<CinemachineCamera>();
+            cinemachine.Follow = overrideTarget ? overrideTarget : transform;
+        }
+    }
+}
