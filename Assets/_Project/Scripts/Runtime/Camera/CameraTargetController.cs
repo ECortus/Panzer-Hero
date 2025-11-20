ee3125b0ba380de09450b87827c3813eaa670131
ECortus <ve.cortus@gmail.com>
2025-11-20 21:00:22 +0200

    update camera

--- Assets/_Project/Scripts/Runtime/Camera/CameraTargetController.cs
+++ Assets/_Project/Scripts/Runtime/Camera/CameraTargetController.cs
@@ -4,19 +4,53 @@ using UnityEditor;
 
 namespace PanzerHero.Runtime
 {
+    [ExecuteAlways]
     public class CameraTargetController : MonoBehaviour
     {
-        [SerializeField] private Transform overrideTarget;
+        [SerializeField] private Transform targetTransform;
+        
+        [Space(5)] 
+        [SerializeField] private float distanceToTarget = 10;
+        [SerializeField] private float heightOffset = 5f;
         
         void Start()
         {
+            Init();
+        }
+
+        void Init()
+        {
+            if (targetTransform == null)
+                return;
+            
+            SetPosition();
             SetCinemachineTarget();
+        }
+
+        void SetPosition()
+        {
+            var targetPosition = targetTransform.position;
+
+            var direction = transform.rotation * -Vector3.forward;
+            targetPosition += direction * distanceToTarget;
+            targetPosition.y += heightOffset;
+            
+            transform.position = targetPosition;
         }
 
         void SetCinemachineTarget()
         {
             var cinemachine = FindAnyObjectByType<CinemachineCamera>();
-            cinemachine.Follow = overrideTarget ? overrideTarget : transform;
+            cinemachine.Follow = transform;
+            cinemachine.ForceCameraPosition(transform.position, transform.rotation);
+        }
+
+        void Update()
+        {
+            if (targetTransform == null)
+                return;
+            
+            SetPosition();
         }
     }
 }
