ddcdd0553b65639757c0aba7e15014069a575bfe
ECortus <ve.cortus@gmail.com>
2025-12-18 03:10:29 +0200

    installer upd

--- Assets/_Project/Scripts/Runtime/Installers/ProjectInstaller.cs
+++ Assets/_Project/Scripts/Runtime/Installers/ProjectInstaller.cs
@@ -1,12 +1,19 @@
-﻿using Zenject;
+﻿using GameDevUtils.Runtime.UI;
+using GameSaveKit.Runtime.Saveable;
+using UnityEngine;
+using Zenject;
 
 namespace PanzerHero.Runtime.Installers
 {
     public class ProjectInstaller : MonoInstaller
     {
+        [SerializeField] private IsolarvDebugUI debugUI;
+        [SerializeField] private SaveableSupervisor saveableSupervisor;
+        
         public override void InstallBindings()
         {
-            
+            Container.Bind<IsolarvDebugUI>().FromComponentInNewPrefab(debugUI).AsSingle().NonLazy();
+            Container.Bind<SaveableSupervisor>().FromComponentInNewPrefab(saveableSupervisor).AsSingle().NonLazy();
         }
     }
 }
