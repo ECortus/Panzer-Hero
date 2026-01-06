d322bbdac2f456f960577aa99455049af2afdd95
ECortus <ve.cortus@gmail.com>
2026-01-06 01:19:28 +0100

    upd 06-01

---
+++ Assets/_Project/Scripts/Runtime/LevelDesign/Triggers/CoinPickupObject.cs
@@ -0,0 +1,14 @@
+﻿using GameDevUtils.Runtime.Triggers;
+using PanzerHero.Runtime.Currency;
+
+namespace PanzerHero.Runtime.LevelDesign.Triggers
+{
+    public class CoinPickupObject : ResourceTriggerObject
+    {
+        protected override void AddResource(float amount)
+        {
+            var manager = CoinsManager.GetInstance;
+            manager.Plus(amount);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Runtime/LevelDesign/Triggers/DiamondPickupObject.cs
@@ -0,0 +1,14 @@
+﻿using GameDevUtils.Runtime.Triggers;
+using PanzerHero.Runtime.Currency;
+
+namespace PanzerHero.Runtime.LevelDesign.Triggers
+{
+    public class DiamondPickupObject : ResourceTriggerObject
+    {
+        protected override void AddResource(float amount)
+        {
+            var manager = DiamondsManager.GetInstance;
+            manager.Plus(amount);
+        }
+    }
+}
