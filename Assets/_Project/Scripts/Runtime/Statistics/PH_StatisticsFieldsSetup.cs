fb85614b330ee3d5e9d9423d1230b49ae063c1f3
ECortus <ve.cortus@gmail.com>
2026-01-30 19:55:25 +0100

    add statistics 2

--- Assets/_Project/Scripts/Runtime/Statistics/PH_StatisticsFieldsSetup.cs
+++ Assets/_Project/Scripts/Runtime/Statistics/PH_StatisticsFieldsSetup.cs
@@ -1,9 +1,19 @@
 ﻿using GameDevUtils.Runtime.UI.Setups.Statistics;
+using UnityEngine;
 
 namespace PanzerHero.Runtime.Statistics
 {
     public class PH_StatisticsFieldsSetup : StatisticsFieldsSetup
     {
-        
+        protected override void InitializeFields()
+        {
+            RegisterCurrentTimeText();
+        }
+
+        void RegisterCurrentTimeText()
+        {
+            var seconds = Time.time;
+            RegisterRegularTimeStat("Play time", seconds);
+        }
     }
 }
